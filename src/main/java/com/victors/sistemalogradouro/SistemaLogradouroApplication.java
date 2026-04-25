c0c2b69e0e7a0e7c0e53e890607482f433a7b6cc
Victor-Suander <victors.empresa@gmail.com>
2026-04-25 19:08:22 -0300

    chore:Configuracao inicial do projeto

---
+++ src/main/java/com/victors/sistemalogradouro/SistemaLogradouroApplication.java
@@ -0,0 +1,16 @@
+package com.victors.sistemalogradouro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Indica que essa é a classe principal do Spring Boot
+@SpringBootApplication
+public class SistemaLogradouroApplication {
+
+    // Método inicial da aplicação
+    public static void main(String[] args) {
+
+        // Inicializa o Spring Boot
+        SpringApplication.run(SistemaLogradouroApplication.class, args);
+    }
+}
---
+++ src/test/java/com/victors/sistemalogradouro/SistemaLogradouroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victors.sistemalogradouro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaLogradouroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
