5463077d86033c60ee39227420592327e9bd8d85
Victor-Suander <victors.empresa@gmail.com>
2026-04-25 20:05:10 -0300

    feat: Criacao do service do uf com validacao de duplicidade

---
+++ src/main/java/com/victors/sistemalogradouro/service/UfService.java
@@ -0,0 +1,30 @@
+package com.victors.sistemalogradouro.service;
+
+import com.victors.sistemalogradouro.entity.Uf;
+import com.victors.sistemalogradouro.repository.UfRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service // onde ficam as regras de negócio
+@RequiredArgsConstructor // Injeta dependências automaticamente
+public class UfService {
+
+    // Repositorio para acessar o banco de dados
+    private final UfRepository ufRepository;
+    // Salva um UF com validacao
+    public Uf salvar(Uf uf) {
+
+        // Verifica se já existe um UF com a mesma sigla
+        ufRepository.findBySigla(uf.getSigla()).ifPresent(existing -> {
+            throw new RuntimeException("UF já cadastado com essa sigla");
+        });
+        // Salva o UF
+        return ufRepository.save(uf);
+    }
+    // Lista todos os UFs
+    public List<Uf> listar() {
+        return ufRepository.findAll();
+    }
+}
